776b04299f039df90cf7e5fd67c03af20c5cc0dd
ajayygurjar <ajay1651@gmail.com>
2025-02-20 22:41:12 +0530

    Making apps error proof try and catch

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -3,28 +3,40 @@ import { useState } from "react";
 const Movie = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false); 
+  const [error, setError] = useState(null);
 
   async function fetchMovie() {
     setIsLoading(true);
+    setError(null);
 
-    
-    const response = await fetch('https://swapi.dev/api/people/1/');
-    const data = await response.json();
+    try {
+      const response = await fetch('https://swapi.dev/api/people/1/');
 
-    
-    const moviePromises = data.films.map(async (filmUrl) => {
-      const filmResponse = await fetch(filmUrl);
-      const filmData = await filmResponse.json();
-      return {
-        id: filmData.episode_id,
-        name: filmData.title, 
-      };
-    });
+      // always do this before paresing data 
+      if (!response.ok) {
+        throw new Error('Something Went Wrong!');
+      }
 
-    
-    const moviesData = await Promise.all(moviePromises);
 
-    setMovies(moviesData);
+      const data = await response.json();
+      
+      const moviePromises = data.films.map(async (filmUrl) => {
+        const filmResponse = await fetch(filmUrl);
+        const filmData = await filmResponse.json();
+        return {
+          id: filmData.episode_id,
+          name: filmData.title, 
+        };
+      });
+
+      const moviesData = await Promise.all(moviePromises);
+
+      setMovies(moviesData);
+
+    } catch (error) {
+      setError(error.message);
+    }
+
     setIsLoading(false); 
   }
 
@@ -35,9 +47,9 @@ const Movie = () => {
       </section>
 
       <section>
-        {isLoading && <p>Loading ...</p>}
-        
-        {!isLoading && movies.length > 0 && (
+        {error && <p>{error}</p>}
+
+        {!isLoading && movies.length > 0 && !error && (
           <ul>
             {movies.map((movie, index) => (
               <li key={index}>{movie.name}</li> 
@@ -46,6 +58,8 @@ const Movie = () => {
         )}
 
         {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
+        
+        {isLoading && <p>Loading ...</p>}
       </section>
     </>
   );
