828b932834a36e8e0190d2cfc81cdeae0c8e5802
ajayygurjar <ajay1651@gmail.com>
2025-02-20 17:09:43 +0530

    Loading the application using promises asyn await

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -2,31 +2,48 @@ import { useState } from "react";
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); 
 
-  function fetchMovie() {
-    fetch('https://swapi.dev/api/people/1/')
-      .then(response => response.json())
-      .then(data => {
-        // Since data is a single object (not an array), you can directly map it to a new array
-        const transformMovies = [{
-          id: data.url, // Use URL as an identifier (or any unique property)
-          name: data.name
-        }];
-        
-        setMovies(transformMovies);
-      });
+  async function fetchMovie() {
+    setIsLoading(true);
+
+    
+    const response = await fetch('https://swapi.dev/api/people/1/');
+    const data = await response.json();
+
+    
+    const moviePromises = data.films.map(async (filmUrl) => {
+      const filmResponse = await fetch(filmUrl);
+      const filmData = await filmResponse.json();
+      return {
+        id: filmData.episode_id,
+        name: filmData.title, 
+      };
+    });
+
+    
+    const moviesData = await Promise.all(moviePromises);
+
+    setMovies(moviesData);
+    setIsLoading(false); 
   }
 
   return (
     <>
       <button type="button" onClick={fetchMovie}>Fetch Movie</button>
-      <ul>
-        {movies.map((movie, index) => (
-          <li key={index}>{movie.name}</li>
-        ))}
-      </ul>
+      
+      {/* Show loader when isLoading is true */}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {movies.map((movie, index) => (
+            <li key={index}>{movie.name}</li> 
+          ))}
+        </ul>
+      )}
     </>
   );
-}
+};
 
 export default Movie;
