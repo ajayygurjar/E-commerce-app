5a1e63df2b6652b27d39bd0b1ef9244f6ad7e1e9
ajayygurjar <ajay1651@gmail.com>
2025-04-03 22:11:40 +0530

    lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-import { useContext } from "react";
+import {createBrowserRouter,RouterProvider,Navigate,} from "react-router-dom";
+import { useContext,Suspense,lazy } from "react";
 
-import ProductPage from "./components/Products/ProductPage";
+
 import { CartProvider } from "./store/cart-context";
 import { CartDisplay } from "./store/cart-display-context";
+import AuthContext from "./store/auth-context";
+
+/*
 import About from "./pages/About";
 import RootLayout from "./components/Layout/RootLayout";
 import ProfilePage from "./components/Profile/ProfilePage";
 import Contact from "./pages/Contact";
 import ProductDetailPage from "./components/Products/ProductDetailPage";
 import LoginPage from "./components/Auth/LoginPage";
-import AuthContext from "./store/auth-context";
+import ProductPage from "./components/Products/ProductPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
+*/
+
+const ProductPage = lazy(() => import("./components/Products/ProductPage"));
+const ProductDetailPage = lazy(() => import("./components/Products/ProductDetailPage"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const ProfilePage = lazy(() => import("./components/Profile/ProfilePage"));
+const LoginPage = lazy(() => import("./components/Auth/LoginPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const Home = lazy(() => import("./pages/Home"));
+const RootLayout = lazy(() => import("./components/Layout/RootLayout"));
 
 function App() {
   const { isLoggedIn } = useContext(AuthContext);
@@ -25,28 +36,32 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <RootLayout />,
+      element: <Suspense fallback={<div>Loading Product Page...</div>}><RootLayout /> </Suspense>,
       errorElement: <ErrorPage />,
       children: [
-        { path: "/", element: <Home /> },
+        { path: "/", element: <Suspense fallback={<div>Loading Home Page...</div>}><Home /></Suspense> },
 
         {
           path: "/store",
           element: isLoggedIn ? (
+            <Suspense fallback={<div>Loading Product Page...</div>}>
             <ProductPage />
+            </Suspense>
           ) : (
             <Navigate to="/login" replace />
           ),
         },
 
-        { path: "/about", element: <About /> },
+        { path: "/about", element:<Suspense fallback={<div>Loading About Page</div>}> <About /> </Suspense>},
 
-        { path: "/contact", element: <Contact /> },
+        { path: "/contact", element: <Suspense fallback={<div>Loading Contact Page</div>}> <Contact /></Suspense> },
 
         {
           path: "/profile",
           element: isLoggedIn ? (
+            <Suspense fallback={<div>Loading Profile...</div>}>
             <ProfilePage />
+            </Suspense>
           ) : (
             <Navigate to="/login" replace />
           ),
@@ -55,16 +70,19 @@ function App() {
         {
           path: `/product/:id`,
           element: isLoggedIn ? (
+          <Suspense fallback={<div>Loading Product...</div>}>
             <ProductDetailPage />
+            </Suspense>
           ) : (
             <Navigate to="/login" replace />
           ),
         },
 
          
-         { path: "/login", element: isLoggedIn ? <Navigate to="/store" replace /> : <LoginPage /> },
+         { path: "/login", element: isLoggedIn ? <Navigate to="/store" replace /> :
+          <Suspense fallback={<div>Loading Login...</div>}> <LoginPage /> </Suspense>},
 
-         { path: "*", element: <NotFoundPage /> },
+         { path: "*", element:<Suspense fallback={<div>Loading...</div>}> <NotFoundPage />  </Suspense>},
       ],
     },
   ]);
